b2c20abe8add4a046b335bcda86108b0485d1ec7
Nikhil Anand <mail@nikhil.io>
2025-12-24 07:39:09 -0600

    Lots of fixes and improvements

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-Uses `goaccess` to generate pretty, **monthly** and **yearly** access logs for a few sites I have deployed in S3/CloudFront.
+Uses `goaccess` to generate pretty, **monthly** and **yearly** access logs for a few sites I have deployed in S3/CloudFront. You can see [some reports here](https://reports.nikhil.io/).
 
 Involves two separate buckets:
 
 1. One that collects all the logs across all sites and has some expiration policy on its objects.
-2. Another that [simply hosts the reports](https://reports.nikhil.io/) 🗂
+2. Another that [simply hosts the reports](https://reports.nikhil.io/).
 
 Requires `bash`, `goaccess` v1.5+, `geoipupdate`, and `awscli` to be installed. As of now, will only work with CloudFront logs since that's all I deal with. Super-easy to adapt to other kinds of logs.
 
@@ -43,15 +43,11 @@ autoreconf -fiv
 
 make
 make install
-
-# Folder setup
-mkdir -p $LOG_ROOT/geoip/
 ```
 
 ### GeoIP Information
 
-You will need [the GeoIP configuration information](https://dev.maxmind.com/geoip/updating-databases?lang=en#2-obtain-geoipconf-with-account-information) from MaxMind.
-Get a MaxMind account and API key and, in this folder,
+You will need [the GeoIP configuration information](https://dev.maxmind.com/geoip/updating-databases?lang=en#2-obtain-geoipconf-with-account-information) from MaxMind. Get a MaxMind account and API key and, in this folder,
 
 ```bash
 mkdir ./geoip
@@ -65,20 +61,20 @@ The script will fail if you don't do this.
 With `aws` and `goaccess` in your `$PATH` (and the GeoIP data in `./geoip`), you need to specify (a) the bucket to pull logs from and (b) the bucket to push generated reports to. Here's a sample invocation:
 
 ```shell
-# In this folder
-BUCKET_FOR_LOGS="logs.example.com" BUCKET_FOR_REPORTS="reports.example.com" ./generate.sh foo.example.com current
+./generate.sh \
+    --reports-bucket reports.example.com \
+    --logs-bucket logs.example.com \
+    --website foo.example.com
 ```
+
+Add `--no-fetch-logs` or `--no-sync-logs` to disable automatic fetching and syncing.
 
 You will need the AWS CLI configured to pull and push to your buckets. Assumed that this is the `default` profile; customize as you see fit.
 
-The logs will be synced to `./foo.example.com/logs` (will be created if it doesn't exist). A `goaccess` database will be created at `./foo.example.com/db` (will be created if it doesn't exist).
+The logs will be downloaded to (or must exist in) `./websites/foo.example.com/logs` (will be created if it doesn't exist). Many `goaccess` databases will be created at `./websites/foo.example.com/db` for incremental reporting.
 
-That `current` part will only generate logs for the current year and month. Omit it to generate logs for whatever you've synced from `BUCKET_FOR_LOGS`.
+👉 Back up the generated reports in `./reports/foo.example.com` !
 
-Here's a sample `cron` job:
+## License
 
-```bash
-0  0  *  *  * pushd /path/to/this/repo; BUCKET_FOR_LOGS="logs.example.com" BUCKET_FOR_REPORTS="reports.example.com" ./generate.sh foo.example.com current; popd
-```
-
-👉 You must back up the generated reports and the `db` folder!
+[WTFPL](https://www.wtfpl.net/)
--- generate.sh
+++ generate.sh
@@ -1,22 +1,88 @@
 #!/usr/bin/env bash
 
-# NOTE: YOU MUST HAVE ALL THE AWS ENV VARS DECLARED BEFORE RUNNING THIS!
-
 set -euo pipefail
 
 # TODO: Sliding window problem (in relation to retention policies)? This is
 #       pretty severe... does the presistence of the goaccess database solve
 #       this problem?
+#
 #       MAYBE: If the current year is not the year, skip it. Reports
 #       have already been generated.
 #
 # TODO: Check the current month and assume that all prior months have been
 #       generated?
 
+REPORTS_BUCKET=""
+LOGS_BUCKET=""
+WEBSITE=""
+FETCH_LOGS=1
+SYNC_LOGS=1
 LOGFILE_TYPE="CLOUDFRONT"
 GEO_IP_DB_LOCATION="./geoip/GeoLite2-Country.mmdb"
 REPORTS_LOCATION="./reports"
 WEBSITES_LOCATION="./websites"
+MONTHS=(
+    "01"
+    "02"
+    "03"
+    "04"
+    "05"
+    "06"
+    "07"
+    "08"
+    "09"
+    "10"
+    "11"
+    "12"
+)
+
+# --- Parse CLI Args ---
+
+usage() {
+    echo "USAGE:"
+    echo "$0 --reports-bucket <bucket> --logs-bucket <bucket> --website <name>"
+    echo ""
+    echo "Bucket names should not be prefixed with s3:// and should not end in /"
+    echo "Just the domain and TLD for the website name (e.g. example.com)"
+    echo ""
+    exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --reports-bucket)
+            REPORTS_BUCKET="${2#*://}"  # strip protocol
+            REPORTS_BUCKET="${REPORTS_BUCKET%/}"  # strip trailing slash
+            shift 2
+        ;;
+        --logs-bucket)
+            LOGS_BUCKET="${2#*://}"
+            LOGS_BUCKET="${LOGS_BUCKET%/}"
+            shift 2
+        ;;
+        --website)
+            WEBSITE="${2#*://}"
+            WEBSITE="${WEBSITE%/}"
+            shift 2
+        ;;
+        --no-fetch-logs)
+            FETCH_LOGS=0
+            shift
+        ;;
+        --no-sync-logs)
+            SYNC_LOGS=0
+            shift
+        ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+        ;;
+    esac
+done
+
+if [[ -z $REPORTS_BUCKET || -z $LOGS_BUCKET || -z $WEBSITE ]]; then
+    usage
+fi
 
 # --- Sanity Checks ---
 
@@ -35,141 +101,106 @@ if [[ ! -f "./geoip/GeoLite2-Country.mmdb" ]]; then
     echo "I use the MaxMind GeoIP Database (the free stuff)"
     echo "Please install it and make sure the databases are in ./geoip"
     echo "Then just run"
-    echo "  mkdir ./geoip"
-    echo "  geoipupdate -d ./geoip"
+    echo ""
+    echo "      mkdir ./geoip"
+    echo "      geoipupdate -d ./geoip"
+    echo ""
     exit 30
 fi
 
-if [[ -z $BUCKET_FOR_LOGS ]]; then
-    echo "You need to set an environment variable that tells me where to fetch the logs from."
-    echo "It's called 'BUCKET_FOR_LOGS' and it's an S3 bucket (without the prefix!)."
-    echo "Here's an example:"
-    echo "  export BUCKET_FOR_LOGS=logs.example.com"
-    exit 40
-fi
+# --- Some basic prep ---
 
-if [[ -z $BUCKET_FOR_REPORTS ]]; then
-    echo "You need to set an environment variable that tells me where to push the generated reports."
-    echo "It's called 'BUCKET_FOR_REPORTS' and it's an S3 bucket (without the prefix!)."
-    echo "Here's an example:"
-    echo "  export BUCKET_FOR_REPORTS=reports.example.com"
-    exit 50
-fi
-
-WEBSITE=""
-if [[ -z $1 ]]; then
-    echo "You need to give me a website that I should generate reports format."
-    echo "This is the first argument you provide to this script."
-    exit 60
-fi
-WEBSITE=$1
-
-# Assumption here is that all logs for months, years past have already been
-# generated.
-
-CURRENT_YEAR=$(date "+%Y")
-CURRENT_MONTH=$(date "+%m")
-if [[ -n $2 ]]; then
-    echo "Only generating reports for $CURRENT_YEAR - $CURRENT_MONTH"
-    MONTHS=("$CURRENT_MONTH")
-    YEARS=("$CURRENT_YEAR")
-else
-    # We just go through all months lazily. `find` is "fast enough". Chill.
-    MONTHS=(
-        "01"
-        "02"
-        "03"
-        "04"
-        "05"
-        "06"
-        "07"
-        "08"
-        "09"
-        "10"
-        "11"
-        "12"
-    )
-
-    # Get the number of unique years we're dealing with. This will only work for
-    # CloudFront logs and that's OK for now...
-    YEARS=($(find "./$WEBSITES_LOCATION/$WEBSITE/logs" -type f -iname "*.gz" | sort | xargs basename | cut -d"." -f2 | cut -d"-" -f1 | sort | uniq))
-fi
+mkdir -p "$WEBSITES_LOCATION/$WEBSITE"/{db,logs}
 
 # --- Fetch Raw Logs ---
 
-echo "Fetching logs for $WEBSITE from $BUCKET_FOR_LOGS"
-mkdir -p ./"$WEBSITES_LOCATION"/"$WEBSITE"/{db,logs}
-aws s3 sync "s3://$BUCKET_FOR_LOGS/$WEBSITE/" "./websites/$WEBSITE/logs/"
+if [[ $FETCH_LOGS -eq 1 ]]; then
+    echo "Fetching logs for $WEBSITE from $LOGS_BUCKET"
+    aws s3 sync "s3://$LOGS_BUCKET/$WEBSITE/" "./$WEBSITES_LOCATION/$WEBSITE/logs/"
+fi
 
-# TODO: Check if there are any logs and exit with a nice message...
+# --- GoAccess function ---
+
+run_goaccess() {
+    local output_location="$1"
+    local title="$2"
+    local db_location="$3"
+
+    goaccess - \
+    --log-format="$LOGFILE_TYPE" \
+    --date-format="$LOGFILE_TYPE" \
+    --time-format="$LOGFILE_TYPE" \
+    --ignore-crawlers \
+    --ignore-status=301 \
+    --ignore-status=302 \
+    --geoip-database="$GEO_IP_DB_LOCATION" \
+    --with-output-resolver \
+    --agent-list \
+    --real-os \
+    --json-pretty-print \
+    --db-path="$db_location" \
+    --persist \
+    --restore \
+    --output="$output_location" \
+    --html-report-title="$title" \
+    --html-custom-css="custom.css"
+}
 
 # --- Generate Reports ---
 
-mkdir -p "$REPORTS_LOCATION/$WEBSITE"
+mapfile -t YEARS < <(
+    find "$WEBSITES_LOCATION/$WEBSITE/logs" -type f -iname "*.gz" |
+    sort |
+    xargs -r basename |
+    cut -d"." -f2 |
+    cut -d"-" -f1 |
+    sort |
+    uniq
+)
 
+echo "Found years:" "${YEARS[@]}"
+
+#
+# Main loop. Copypasta is OK. Breathe.
+#
 for YEAR in "${YEARS[@]}"; do
-    mkdir -p "$REPORTS_LOCATION/$WEBSITE/$YEAR"
-
-    # Generate yearly logs
     echo "Generating logs for $YEAR"
+
     mkdir -p "$REPORTS_LOCATION/$WEBSITE/$YEAR"
     cp -v ./custom.css "$REPORTS_LOCATION/$WEBSITE/$YEAR/"
+    mkdir -p "$WEBSITES_LOCATION/$WEBSITE/db/$YEAR"
 
-    find "./$WEBSITES_LOCATION/$WEBSITE/logs" -type f -iname "*$YEAR*.gz" -exec gunzip -c {} \; |
-        goaccess - \
-            --log-format="$LOGFILE_TYPE" \
-            --date-format="$LOGFILE_TYPE" \
-            --time-format="$LOGFILE_TYPE" \
-            --ignore-crawlers \
-            --ignore-status=301 \
-            --ignore-status=302 \
-            --geoip-database="$GEO_IP_DB_LOCATION" \
-            --with-output-resolver \
-            --agent-list \
-            --real-os \
-            --json-pretty-print \
-            --db-path="./$WEBSITES_LOCATION/$WEBSITE/db" \
-            --persist \
-            --output="./$REPORTS_LOCATION/$WEBSITE/$YEAR/index.html" \
-            --html-report-title="$WEBSITE - $YEAR" \
-            --html-custom-css="custom.css"
+    find "$WEBSITES_LOCATION/$WEBSITE/logs" -type f -iname "*.$YEAR-*.gz" -exec gunzip -c {} \; |
+    run_goaccess \
+    "$REPORTS_LOCATION/$WEBSITE/$YEAR/index.html" \
+    "$WEBSITE - $YEAR" \
+    "$WEBSITES_LOCATION/$WEBSITE/db/$YEAR"
 
+    # Now do months. Whichever ones you find that is.
     for MONTH in "${MONTHS[@]}"; do
+        mapfile -t LOGS < <(find "$WEBSITES_LOCATION/$WEBSITE/logs" -type f -iname "*.$YEAR-$MONTH*.gz")
 
-        # Just be relatively 'clean' and only generate logs for the month you
-        # find. This also SHOULD NOT TOUCH the reports that have been generated
-        # before when you generate/sync!
-        LOGS=($(find "./$WEBSITES_LOCATION/$WEBSITE/logs" -type f -iname "*$YEAR-$MONTH*.gz"))
         if [[ ${#LOGS[@]} -ne 0 ]]; then
             echo "Generating logs for $YEAR - $MONTH"
+
             mkdir -p "$REPORTS_LOCATION/$WEBSITE/$YEAR/$MONTH"
             cp -v ./custom.css "$REPORTS_LOCATION/$WEBSITE/$YEAR/$MONTH/"
+            mkdir -p "$WEBSITES_LOCATION/$WEBSITE/db/$YEAR-$MONTH"
 
-            # Copypasta is OK. Breathe.
-            find "./$WEBSITES_LOCATION/$WEBSITE/logs" -type f -iname "*$YEAR-$MONTH*.gz" -exec gunzip -c {} \; |
-                goaccess - \
-                    --log-format="$LOGFILE_TYPE" \
-                    --date-format="$LOGFILE_TYPE" \
-                    --time-format="$LOGFILE_TYPE" \
-                    --ignore-crawlers \
-                    --ignore-status=301 \
-                    --ignore-status=302 \
-                    --geoip-database="$GEO_IP_DB_LOCATION" \
-                    --with-output-resolver \
-                    --agent-list \
-                    --real-os \
-                    --db-path="./$WEBSITES_LOCATION/$WEBSITE/db" \
-                    --persist \
-                    --output="./$REPORTS_LOCATION/$WEBSITE/$YEAR/$MONTH/index.html" \
-                    --html-report-title="$WEBSITE - $YEAR - $MONTH" \
-                    --html-custom-css="custom.css"
+            find "$WEBSITES_LOCATION/$WEBSITE/logs" -type f -iname "*.$YEAR-$MONTH*.gz" -exec gunzip -c {} \; |
+            run_goaccess \
+            "$REPORTS_LOCATION/$WEBSITE/$YEAR/$MONTH/index.html" \
+            "$WEBSITE - $YEAR/$MONTH" \
+            "$WEBSITES_LOCATION/$WEBSITE/db/$YEAR-$MONTH"
         fi
     done
 done
 
 # --- Sync Reports ---
 
-# Note that we're not deleting anything here! Keep the old stuff in place! AND
-# TAKE BACKUPS OF THIS BUCKET! What it contains is the 'precipitate' of all the
-# raw logs.
-aws s3 sync "./$REPORTS_LOCATION/$WEBSITE/" "s3://$BUCKET_FOR_REPORTS/$WEBSITE/"
+# Note that we're not deleting anything here. That's for another process. Take
+# backups of this reports bucket! What it contains is the 'precipitate' of all
+# the raw logs.
+if [[ $SYNC_LOGS -eq 1 ]]; then
+    aws s3 sync "$REPORTS_LOCATION/$WEBSITE/" "s3://$REPORTS_BUCKET/$WEBSITE/"
+fi
